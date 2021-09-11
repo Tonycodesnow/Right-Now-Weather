7b6e1ac8221cf97bef3e303e4ce79e86566420e7
Ant <huertinez.family@outlook.com>
2021-09-11 00:27:45 -0700

    added current weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var cities = []
 // function for city search
 function citySearch() {
     var cityName = $("#citySearchInput").val()
-    fetch(weatherURL+cityName+"&apiKey="+apiKey).then.
+    // fetch(weatherURL+cityName+"&apiKey="+apiKey).then.
 }
 
 // store city search list to loco storage
@@ -17,7 +17,29 @@ function saveSearchCity(city) {
 
 // gets current weather and uv index 
 
+var currentWeather = function(cityName) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial')
+    .then(function(res) {
+        return res.json();
+    }
+    .then(function(data) {
 
+        console.log("current weather: ", data);
+        const template = `
+        <h2 class="subtitle col-md-6">${data.name} (${new Date().toLocaleDateString()})</h2>
+        <i class=icon col-md-4><img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png"/></i>
+        
+        <ul>
+            <li>Temp: ${data.main.temp}</li>
+            <li>Wind: ${data.wind.speed}</li>
+            <li>Humidity: ${data.main.humidity}</li>
+            <li>UV Index</li>
+        </ul>
+        `
+
+        document.querySelector("#today").innerHTML = template;
+    }
+};
 
 
 // api function for city
@@ -28,5 +50,3 @@ function saveSearchCity(city) {
 // 5 day forecast for current city
 
 
-
- 
